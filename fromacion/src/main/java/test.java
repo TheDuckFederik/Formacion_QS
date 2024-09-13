a7594208d902732364cf9415ad7f98f4d065e562
UnOvPj <unai.ovejero@iaballester.cat>
2024-09-13 09:18:32 +0200

    00005

---
+++ fromacion/src/main/java/test.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class test {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/unai.ovejero.ext/Downloads/chromedriver-win64/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        String url = "https://www.demoqa.com/";
+        driver.get(url);
+
+        WebElement element = driver.findElement(By.className("test"));
+
+        String text = element.getText();
+
+        System.out.println(text);
+
+        driver.quit();
+    }
+}

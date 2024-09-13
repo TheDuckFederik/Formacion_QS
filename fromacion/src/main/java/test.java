857497d19d22e754b882e9d19df6c4a0e189333f
UnOvPj <unai.ovejero@iaballester.cat>
2024-09-13 10:20:38 +0200

    00007

--- fromacion/src/main/java/test.java
+++ fromacion/src/main/java/test.java
@@ -1,19 +1,14 @@
-//import org.openqa.selenium.By;
+//
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+//
 public class test {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        //
         String url = "https://www.demoqa.com/";
         driver.get(url);
-
-        // WebElement element = driver.findElement(By.className("Main"));
-
-        // String text = element.getText();
-
-        // System.out.println(text);
-
+        //
         driver.quit();
     }
 }

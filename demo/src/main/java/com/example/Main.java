84eceaeeeeb172d8a0590a61589b35064c37b1db
Ramon-Fernandez <ramon.fernandez-roig.ext@inetum.com>
2024-09-13 09:28:54 +0200

    git pull

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,6 +2,33 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("start-maximized");
+        chromeOptions.addArguments("--log-level=1");
+        chromeOptions.addArguments("--disable-notifications");
+        chromeOptions.addArguments("--disable-search-engine-choice-screen");
+        chromeOptions.setAcceptInsecureCerts(true);
+        chromeOptions.addArguments("--incognito");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        try{
+
+            driver.get("https://www.wikipedia.es");
+            Thread.sleep(2000);
+            WebElement menu = driver.findElement(By.id("vector-main-menu-dropdown-checkbox"));
+            menu.click();
+            Thread.sleep(2000);
+            WebElement pagAleatoria =  driver.findElement(By.id("n-randompage"));
+           pagAleatoria.click();
+            Thread.sleep(5000);
+            String titulo = driver.getCurrentUrl();
+
+            System.out.println(titulo);
+
+        } catch(Exception e){
+            e.printStackTrace();
+        } finally{
+            driver.quit();
+        }
     }
 }
--- seleniumprincipio/seleniumprincipio/src/main/java/com/example/Main.java
+++ seleniumprincipio/seleniumprincipio/src/main/java/com/example/Main.java
@@ -21,18 +21,24 @@ public class Main {
         WebDriver driver = new ChromeDriver(chromeOptions);
         try{
 
-            driver.get("https://www.wikipedia.es");
+            driver.get("https://demoqa.com");
             Thread.sleep(2000);
-            WebElement menu = driver.findElement(By.id("vector-main-menu-dropdown-checkbox"));
+            WebElement menu = driver.findElement(By.xpath("//h5[normalize-space()='Elements']"));
             menu.click();
             Thread.sleep(2000);
-            WebElement pagAleatoria =  driver.findElement(By.id("n-randompage"));
-           pagAleatoria.click();
+            WebElement submenu = driver.findElement(By.id("item-0"));
+            submenu.click();
+            Thread.sleep(2000);
+            WebElement userName = driver.findElement(By.id("userName"));
+            userName.sendKeys("Holaquetal");
+            Thread.sleep(2000);
+            WebElement email = driver.findElement(By.id("userEmail"));
+            email.sendKeys("EmailMalo");
+            Thread.sleep(2000);
+            WebElement submit = driver.findElement(By.id("submit"));
+            submit.click();
             Thread.sleep(5000);
-            String titulo = driver.getCurrentUrl();
-
-            System.out.println(titulo);
-
+            
         } catch(Exception e){
             e.printStackTrace();
         } finally{

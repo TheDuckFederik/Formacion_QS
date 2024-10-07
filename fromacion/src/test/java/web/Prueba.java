b29489ed7722e1a319a82c13c84db8e8008f7ea4
UnOvPj <unai.ovejero@iaballester.cat>
2024-10-07 12:23:44 +0200

    00046

--- fromacion/src/test/java/web/Prueba.java
+++ fromacion/src/test/java/web/Prueba.java
@@ -182,6 +182,81 @@ public class Prueba extends Main {
     @Story("Elements")
     @Description("Rellenar textbox")
     //
+    public void JSON_Test() throws InterruptedException, IOException {
+        testId = "JSON_Test";
+        try {
+            //
+            System.out.println("|-------------------------------------------------------------------|");
+            System.out.println("|------------------------ Beginning JSON_Test -----------------------|");
+            System.out.println("|-------------------------------------------------------------------|");
+            //
+            action = 0;
+            //
+            String fullNameBox = getJsonValue("fullNameText_TC_001");
+            String emailBox = getJsonValue("emailText_TC_001");
+            String currentAddressBox = getJsonValue("currentAddressText_TC_001");
+            String permanentAddressBox = getJsonValue("permanentAddressText_TC_001");
+            //
+            driver.get("https://demoqa.com");
+            action = 1;
+            //
+            WebElement menu = driver.findElement(By.xpath("(//div[@class='avatar mx-auto white'])[1]"));
+            menu.click();
+            action = 2;
+            //
+            WebElement subMenu = driver.findElement(By.xpath("//div[@class='element-list collapse show']//li[@id='item-0']"));
+            subMenu.click();
+            action = 3;
+            //
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("window.scrollBy(0,350)", "");
+            Thread.sleep(500);
+            action = 4;
+            //
+            WebElement full_name = driver.findElement(By.id("userName"));
+            full_name.sendKeys(fullNameBox);
+            Thread.sleep(50);
+            action = 5;
+            //
+            WebElement email = driver.findElement(By.id("userEmail"));
+            email.sendKeys(emailBox);
+            Thread.sleep(50);
+            action = 6;
+            //
+            WebElement current_address = driver.findElement(By.id("currentAddress"));
+            current_address.sendKeys(currentAddressBox);
+            Thread.sleep(50);
+            action = 7;
+            //
+            WebElement permanent_address = driver.findElement(By.id("permanentAddress"));
+            permanent_address.sendKeys(permanentAddressBox);
+            Thread.sleep(50);
+            action = 8;
+            //
+            WebElement submit = driver.findElement(By.id("submit"));
+            submit.click();
+            action = 9;
+            //
+            WebElement output_text = driver.findElement(By.xpath("(//div[@class='border col-md-12 col-sm-12'])[1]"));
+            String print_output = output_text.getText();
+            System.out.println(print_output);
+            action = 10;
+            //
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+            //
+            System.out.println("|-------------------------------------------------------------------|");
+            System.out.println("|-------- JSON_Test stopped after performing action " + action + " of 10 --------|");
+            System.out.println("|-------------------------------------------------------------------|");
+            //
+        }
+    }
+    @Test(description = "Prueba DemoQA Elements Text Box")
+    @Story("Elements")
+    @Description("Rellenar textbox")
+    //
     public void TC001_Text_Box() throws InterruptedException, IOException {
         testId = "TC001_Text_Box";
         //
